abcffc79beb4cc4663954fc7a4c4141131cd2b5a
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-13 23:15:22 +0700

    chore(pingCommand): change message to embed

--- src/commands/Util/PingCommand.ts
+++ src/commands/Util/PingCommand.ts
@@ -1,6 +1,7 @@
 import { CommandOptions, Command } from "@sapphire/framework";
 import { ApplyOptions } from "@sapphire/decorators";
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
+import timezone from "moment-timezone";
 
 @ApplyOptions<CommandOptions>({
     name: "ping",
@@ -8,8 +9,22 @@ import { Message } from "discord.js";
 })
 
 export class PingCommand extends Command {
-    async run(message: Message) {
-        const msg = await message.channel.send("Pong");
-        await msg.edit(`:ping_pong: | ${msg.createdTimestamp - message.createdTimestamp} ms, ${this.container.client.ws.ping} ms`);
+    async run(msg: Message) {
+        try {
+            const message = await msg.channel.send({ content : "Getting info..." });
+            
+            const embed = new MessageEmbed()
+                .addField(`⏳ Latency `, `__**${message!.createdTimestamp - msg.createdTimestamp} ms**__`)
+                .addField("💓 Websocket", `__**${Math.floor(this.container.client.ws.ping)} ms**__`)
+                .setColor("#303136")
+                .setFooter(`Requested by ${msg.author.username} | Today at ${timezone.tz("Asia/Jakarta").format("HH:mma") + " "}`, msg.author.displayAvatarURL({
+                dynamic: true
+            }))
+            setTimeout(function() { message!.edit({content: ' ', embeds: [embed] }) }, 5000);
+        } catch(e) {
+            const embed = new MessageEmbed()
+                .setDescription(`${e}`)
+            msg.channel.send({ embeds: [embed] })
+        }
     }
 }
