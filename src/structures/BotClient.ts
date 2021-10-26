429baddaa5aac1dce4c843c10071b82adc6fb261
Munz1211 <64169740+Munz1211@users.noreply.github.com>
2021-10-26 18:19:05 +0700

    chore(BotXlient): Change BotClient

--- src/structures/BotClient.ts
+++ src/structures/BotClient.ts
@@ -1,5 +1,5 @@
 import { SapphireClient, SapphireClientOptions } from "@sapphire/framework";
-import { Intents } from "discord.js";
+import { Intents, LimitedCollection, MessageEmbed, Options } from "discord.js";
 import { join } from "path";
 import { botPrefix } from "../config";
 import { tempVoiceManager } from "../tempVoiceManager";
@@ -7,6 +7,8 @@ import { tempVoiceManager } from "../tempVoiceManager";
 export class BotClient extends SapphireClient {
     public constructor(clientOptions?: SapphireClientOptions) {
         super({
+            restTimeOffset: 0,
+            restWsBridgeTimeout: 100,
             allowedMentions: {
                 users: [],
                 repliedUser: false
@@ -16,6 +18,15 @@ export class BotClient extends SapphireClient {
             caseInsensitivePrefixes: true,
             defaultPrefix: botPrefix,
             intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
+            makeCache: Options.cacheWithLimits({
+                MessageManager: {
+                  sweepInterval: 300,
+                  sweepFilter: LimitedCollection.filterByLifetime({
+                    lifetime: 60,
+                    getComparisonTimestamp: e => e.editedTimestamp ?? e.createdTimestamp,
+                  })
+                }
+              }),
             ...clientOptions
         });
     }
